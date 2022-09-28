15e2f00b7bbe112abb0988456645be31ff7fc1fd
Stanislav Gil <stasgill@gmail.com>
2022-09-28 17:51:24 +0300

    add mobile menu

--- src/components/hero/Header.tsx
+++ src/components/hero/Header.tsx
@@ -1,10 +1,13 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { Icon } from "../icon/Icon";
 import { HeaderProps } from "./types";
 
 export const Header: FunctionComponent<HeaderProps> = ({
   logo,
   navigation,
 }) => {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <div className="header__container">
       <div className="logo">
@@ -17,6 +20,28 @@ export const Header: FunctionComponent<HeaderProps> = ({
           </a>
         ))}
       </div>
+      <div
+        className="header__container_burger"
+        onClick={() => setVisible(true)}
+      >
+        <Icon variant="burger" fill="white" />
+      </div>
+      {isVisible && (
+        <div className="burger-menu">
+          <div className="burger-menu_close" onClick={() => setVisible(false)}>
+            <Icon variant="close" fill="white" />
+          </div>
+          {navigation.map((item) => (
+            <a
+              href={`#${item.id}`}
+              key={item.id}
+              onClick={() => setVisible(false)}
+            >
+              {item.title}
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
